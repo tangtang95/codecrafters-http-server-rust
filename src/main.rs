bc1ffdfe79f4016db16266e0757582d166f0c07f
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2023-10-21 11:16:26 +0200

    feat: parse first line of http request

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,81 @@
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Write, Read, BufReader, BufRead};
+use nom::IResult;
+use nom::bytes::complete::{tag, take_until};
 
+use anyhow::Result;
 
-fn main() {
+#[derive(Debug)]
+struct HttpRequest {
+    command: HttpCommand
+}
+
+#[derive(Debug)]
+struct HttpCommand {
+    method: String,
+    path: String,
+    version: String,
+}
+
+fn http_request(input: &str) -> IResult<&str, HttpRequest> {
+    let (input, command) = take_until("\r\n")(input)?;
+    let (_, command) = http_command(command)?;
+    let (input, _) = tag("\r\n")(input)?;
+    Ok((input, HttpRequest {
+        command
+    }))
+}
+
+fn http_command(input: &str) -> IResult<&str, HttpCommand> {
+    let (input, method) = take_until(" ")(input)?;
+    let (input, _) = tag(" ")(input)?;
+    let (input, path) = take_until(" ")(input)?;
+    let (input, _) = tag(" ")(input)?;
+    let (input, version) = take_until("\r\n")(input)?;
+
+    Ok((input, HttpCommand {
+        method: method.to_string(),
+        path: path.to_string(),
+        version: version.to_string()
+    }))
+}
+
+fn main() -> Result<()>{
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
     
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                let _ = stream.write(response.as_bytes());
-                stream.flush().unwrap();
+                
+                let mut reader = BufReader::new(stream.try_clone()?);
+                let mut buf = String::new();
+                reader.read_line(&mut buf)?;
+                let (_, request) = http_request(&buf).unwrap();
+                
+                eprintln!("request: {:?}", request);
+                match request.command.path.as_str() {
+                   "/"  => {
+                        let response = "HTTP/1.1 200 OK\r\n\r\n";
+                        
+                        stream.write(response.as_bytes())?;
+                        stream.flush()?;
+                   },
+                   _ => {
+                        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                        
+                        stream.write(response.as_bytes())?;
+                        stream.flush()?;
+                   }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
